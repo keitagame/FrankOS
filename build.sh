852a0bc513abf32d9f8a3d550cad0a11ae8ee32a
keita <keitagame0319@icloud.com>
2025-08-16 09:26:08 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,12 @@ ISO=dist/frankos-minimal.iso
 # クリーン
 rm -rf dist
 mkdir -p $ROOTFS
+# rootfs を pacman で直接構築（pacstrap 非依存）
+mkdir -p "$ROOTFS"/var/lib/pacman
+pacman -Sy --root "$ROOTFS" --noconfirm base linux bash vim
 
 # 1) pacman で rootfs をブートストラップ
-pacstrap -c "$ROOTFS" base linux bash vim
+# pacstrap -c "$ROOTFS" base linux bash vim
 
 # 2) pacman.conf をコピー（必要に応じてカスタマイズ）
 mkdir -p $ROOTFS/etc
